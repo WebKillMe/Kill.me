fa11abea187335b3e6356a34b780f0abe1d7c72e
rafaleonruiz <rafaleonruiz@gmail.com>
2026-06-20 12:52:31 +0200

    Fix service worker cache so updates show immediately

--- sw.js
+++ sw.js
@@ -1,12 +1,14 @@
-const CACHE = "vudureal-v1";
+const CACHE = "vudureal-v3";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icons/icon-192.png", "./icons/icon-512.png"];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
 });
 
+self.addEventListener("activate", (e) => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+});
+
 self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((cached) => cached || fetch(e.request))
-  );
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
