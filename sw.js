dd56e173560c9f9802579836a5d7eae147bf9a92
rafaleonruiz <rafaleonruiz@gmail.com>
2026-06-20 12:09:13 +0200

    Initial commit

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = "vudureal-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icons/icon-192.png", "./icons/icon-512.png"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
+  );
+});
